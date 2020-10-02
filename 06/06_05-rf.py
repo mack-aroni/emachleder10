05b1920ea5accbc8912b1411c7bd37e69a9a5173
mack-aroni <71650717+mack-aroni@users.noreply.github.com>
2020-10-02 08:06:02 -0400

    Add files via upload

---
+++ 06/06_05-rf.py
@@ -0,0 +1,31 @@
+'''
+Team "Dean and Ethan"
+Roster: Ethan Machleder, Dean Carey, Ethan Shenker
+SoftDev
+K06: Learnination Through Amalgamation
+2020-10-1
+'''
+
+#Write a program that will print the name of a randomly-selected student from team (orpheus, rex, or endymion).
+
+import random
+
+def pick():
+    KREWES = {'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA',
+                          'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+              'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE',
+                      'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+              'endymion': ['JASON', 'DEAN', 'MADDIE', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY',
+                           'ERIN', 'MEIRU']}
+    team = input("Enter team name: ").lower()
+    #picks one of the group members
+    print(random.choice(KREWES.get(team)))
+
+pick()
+
+'''
+We all agreed that the question is asking to randomly choose a student from a random team. So we created a list
+containing the three keys in KREWES and randomly picked a team's position. Then we randomly picked a student's
+position in that team. When we print KREWES[teams[r]][pos], it randomly chooses a key/team in KREWES, and prints
+one of its values/students at random.
+'''
---
+++ 07/07_py-csv.py
@@ -0,0 +1,30 @@
+'''
+Team "Dean and Ethan"
+Roster: Ethan Machleder, Dean Carey, Ethan Shenker
+SoftDev
+K07: StI/O: Divine your Destiny!
+2020-10-1
+'''
+
+import random
+
+def create_dict():
+    #open the file
+    file = open("occupations.csv")
+    #create dictionary
+    new_dict = {}
+    #split lines into key, value by last comma
+    for line in file: 
+        key,val = line.strip().rsplit(',',1)
+        new_dict[key] = val
+    #pop the first and last lines
+    new_dict.pop("Job Class")
+    new_dict.pop("Total")
+    #create a list of the keys, and weights
+    pop_list = list(new_dict.keys())
+    weight_list = [float(a) for a in list(new_dict.values())]
+    #use randomchoices to pick a weighted item
+    print(random.choices(pop_list, weight_list, k = 1)[0])
+    file.close() #Closes file
+
+create_dict()
